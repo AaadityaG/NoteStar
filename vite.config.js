f037311e67f235cd78ca6e524691abb4466c42dd
Aditya <helloadig@gmail.com>
2024-11-08 13:12:05 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    },
+      outDir: 'dist',
+      target: 'esnext',
+      rollupOptions: {
+        input: 'src/index.html'
+      }
+    },  
   plugins: [react()],
 })
